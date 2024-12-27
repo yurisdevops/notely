18772a54fd791658ba38379f44c6aa2d68077f13
yurisdevops <yurimachado.dev@gmail.com>
2024-12-27 18:27:51 -0300

    Atualização do auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,12 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [Google],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
