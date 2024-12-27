863f71aea40c646b684ebdd64620437fcaa6370e
yurisdevops <yurimachado.dev@gmail.com>
2024-12-26 21:33:51 -0300

    Atualização o task page.txs

--- src/app/task/[id]/page.tsx
+++ src/app/task/[id]/page.tsx
@@ -1,3 +1,4 @@
+// src/app/task/[id]/page.tsx
 import { Metadata } from "next";
 import TaskContent from "./TaskContent"; // Verifique se o caminho do import está correto
 import { auth } from "@/auth";
@@ -7,27 +8,19 @@ export const metadata: Metadata = {
   description: "Acesse os detalhes de sua tarefa.",
 };
 
-interface Params {
-  id: string;
-}
-
-export default async function Task({ params }: { params: Params }) {
+export default async function Task({ params }: { params: { id: string } }) {
+  // Autenticação do usuário
   const session = await auth();
 
-  // Redirecionando se o usuário não estiver autenticado
-  if (!session) {
-    // Pode usar o redirect do Next.js se necessário
-    return <div>Você não está autenticado</div>;
-  }
-
+  // Defina o usuário
   const user = {
-    email: session.user?.email ?? "",
-    name: session.user?.name ?? "Usuário",
+    email: session?.user?.email ?? "",
+    name: session?.user?.name ?? "Usuário",
   };
 
   return (
     <div>
-      <TaskContent params={{ id: params.id }} user={user} allComments={[]} />{" "}
+      <TaskContent params={{ id: params.id }} user={user} allComments={[]} />
     </div>
   );
 }
