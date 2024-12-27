e792696b0222557222c538e66fdbf1343f4293b3
Yuri Souza <143803323+yurisdevops@users.noreply.github.com>
2024-12-27 08:41:09 -0300

    Update page.tsx

--- src/app/task/[id]/page.tsx
+++ src/app/task/[id]/page.tsx
@@ -1,33 +1,24 @@
-// src/app/task/[id]/page.tsx
 import { Metadata } from "next";
-import TaskContent from "./TaskContent"; // Verifique se o caminho do import está correto
-import { auth } from "@/auth";
+import TaskContainer from "./taskContainer"; // O caminho deve ser correto
 
 export const metadata: Metadata = {
   title: "Detalhes da tarefa | Notely",
   description: "Acesse os detalhes de sua tarefa.",
 };
 
-// Tipo genérico para PageProps
-interface PageProps {
-  params: {
-    id: string;
-  };
+interface Params {
+  id: string;
 }
 
-export default async function Task({ params }: PageProps) {
-  // Autenticação do usuário
-  const session = await auth();
+interface Props {
+  params: Params;
+}
 
-  // Definindo o usuário
-  const user = {
-    email: session?.user?.email ?? "",
-    name: session?.user?.name ?? "Usuário",
-  };
-
+export default async function Task({ params }: Props) {
+  console.log("Params recebidos:", params.id);
   return (
     <div>
-      <TaskContent params={{ id: params.id }} user={user} allComments={[]} />
+      <TaskContainer params={params} />{" "}
     </div>
   );
 }
